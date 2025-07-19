8a68158d9c1c3afd284ae91e44b9515eeed8e200
Shubham Kaudewar <you@example.com>
2025-07-19 19:39:33 +0530

    WIP

--- src/service/splitService.py
+++ src/service/splitService.py
@@ -3,12 +3,29 @@ from dataclasses import dataclass
 
 from typing_extensions import override
 
-from src.models.classModels import UserShareMap, UserList
+from src.models.classModels import UserShareMap, UserList, SplitStrategy
 
 class Split(ABC):
     @abstractmethod
     def calculate_split(self, amount: float, participants: UserList, split_details: UserShareMap) -> UserShareMap:
         pass
+
+    def create_split(self, strategy: str) -> UserShareMap:
+        if strategy == SplitStrategy.EQUAL:
+            return EqualSplit().calculate_split(
+                amount=100,
+                participants=[],
+                split_details={}
+            )
+        elif strategy == SplitStrategy.PERCENTAGE:
+            return PercentageSplit().calculate_split(
+                amount=100,
+                participants=[],
+                split_details={}
+            )
+        else:
+            raise Exception("strategy not implemented!")
+
 
 @dataclass
 class PercentageSplit(Split):
@@ -29,7 +46,7 @@ class EqualSplit(Split):
         split: UserShareMap = {}
 
         per_head_amount = amount / len(participants)
-        for user in split_details:
+        for user in participants:
             split[user] = per_head_amount
         return split
 
